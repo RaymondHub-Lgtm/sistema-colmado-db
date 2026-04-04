93f7f9396eb1243c8b433c3f8bc6223d14c52848
RaymondHub-Lgtm <raymondismael@icloud.com>
2026-04-04 22:58:01 +0200

    Create inserts.sql

---
+++ sql/inserts.sql
@@ -0,0 +1,9 @@
+-- datos iniciales
+
+INSERT INTO clientes (nombre, telefono) VALUES
+('Luis', '809-000-0000'),
+('Maria', '829-111-1111');
+
+INSERT INTO productos (nombre, precio, stock) VALUES
+('Arroz', 50, 100),
+('Habichuelas', 60, 80);
