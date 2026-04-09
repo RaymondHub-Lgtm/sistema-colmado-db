281c03bd2cedfb0e12635aa2e4c1e5114a9fa66d
angelmgibbs-blip <angelmgibbs@gmail.com>
2026-04-09 19:22:49 -0400

    Update fdw.sql

--- sql/fdw.sql
+++ sql/fdw.sql
@@ -1,1 +1,21 @@
+EXTENSION
+CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
+-- SERVIDOR REMOTO
+CREATE SERVER servidor_remoto
+FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (host 'localhost', dbname 'colmadodb', port '5432');
+
+-- USUARIO
+CREATE USER MAPPING FOR postgres
+SERVER servidor_remoto
+OPTIONS (user 'postgres', password '1234');
+
+-- IMPORTAR TABLA
+IMPORT FOREIGN SCHEMA public
+LIMIT TO (clientes)
+FROM SERVER servidor_remoto
+INTO public;
+
+-- CONSULTA REMOTA
+SELECT * FROM clientes;
